00cb8df919fbd3b44d123403155475135403df80
tomek7667 <git@cyber-man.pl>
2025-04-13 22:59:35 +0200

    feat: add TODO comments for implementing contests finish and teams ranking endpoints

--- src/backend/internal/openctf/teams.go
+++ src/backend/internal/openctf/teams.go
@@ -11,6 +11,7 @@ func (h *Handler) AddRoutes_ApiTeams() {
 
 	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/:teamId", ratelimit.InMemoryOptions{}, h.TeamsGetOne)
 	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/list", ratelimit.InMemoryOptions{}, h.TeamsList)
+	// h.RestClient.AddRateLimitedRoute("GET", "/api/teams/ranking", ratelimit.InMemoryOptions{}, h.TeamsRanking) // TODO: before this, create view with aggregated scores for each team each year; this endpoint will receive data from this view; filtered out by provided query params
 	h.RestClient.AddRateLimitedRoute("POST", "/api/teams/create", ratelimit.InMemoryOptions{}, h.WithAuth(h.TeamsCreate))
 	h.RestClient.AddRateLimitedRoute("POST", "/api/teams/verify", ratelimit.InMemoryOptions{}, h.WithAuth(h.TeamsVerify))
 }
