9c20fa009a8b02b6492a7376478c26f434409f38
tomek7667 <git@cyber-man.pl>
2025-08-11 04:07:56 +0200

    Improve error handling in AuthVerify function for email verification failures

--- src/backend/internal/openctf/auth_verify.go
+++ src/backend/internal/openctf/auth_verify.go
@@ -1,6 +1,7 @@
 package openctf
 
 import (
+	"fmt"
 	"net/http"
 
 	"openctfbackend/internal/rest"
@@ -21,6 +22,14 @@ func (h *Handler) AuthVerify(ctx *gin.Context) {
 		return
 	}
 	user, token, err := h.ServiceClient.VerifyEmail(ctx, &dto)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, map[string]any{
+			"success": false,
+			"message": fmt.Errorf("failed to verify e-mail with code'%s': %w", dto.Code, err),
+			"data":    nil,
+		})
+		return
+	}
 	rest.FailOrReturn(ctx, map[string]any{
 		"user":  user,
 		"token": *token,
