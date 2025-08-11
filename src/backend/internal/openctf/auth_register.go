2d1fd4ef8813a15929a6cc392ee5943be9736d77
tomek7667 <git@cyber-man.pl>
2025-08-11 03:13:23 +0200

    Update logo image width in confirmation email template

--- src/backend/internal/openctf/auth_register.go
+++ src/backend/internal/openctf/auth_register.go
@@ -51,7 +51,7 @@ func (h *Handler) sendConfirmEmail(user *ent.User) error {
   <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" style="width:600px; margin:0 auto;">
     <tr>
       <td style="padding:24px 16px; text-align:center;">
-        <img src="cid:openctf-logo.png" alt="OpenCTF" width="40" height="40" style="display:inline-block; border:0; outline:none; text-decoration:none;">
+        <img src="cid:openctf-logo.png" alt="OpenCTF" width="107" height="40" style="display:inline-block; border:0; outline:none; text-decoration:none;">
       </td>
     </tr>
 
