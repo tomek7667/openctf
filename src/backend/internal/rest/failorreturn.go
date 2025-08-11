1a366e284280e7243acce641459c7915ea945c26
tomek7667 <git@cyber-man.pl>
2025-08-11 04:05:07 +0200

    Remove CORS handling from FailOrReturn function

--- src/backend/internal/rest/failorreturn.go
+++ src/backend/internal/rest/failorreturn.go
@@ -2,22 +2,13 @@ package rest
 
 import (
 	"net/http"
-	"slices"
 	"strings"
-
-	"openctfbackend/internal/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FailOrReturn(ctx *gin.Context, output any, err error, message ...string) {
 	if err != nil {
-		origin := ctx.GetHeader("Origin")
-		allowedOrigins := strings.Split(utils.Getenv("ALLOWED_ORIGINS", "http://rce.wtf:3000,http://127.0.0.1:3000"), ",")
-		if slices.Contains(allowedOrigins, origin) {
-			ctx.Header("Access-Control-Allow-Origin", origin)
-			ctx.Header("Access-Control-Allow-Credentials", "true")
-		}
 		ctx.JSON(http.StatusInternalServerError, map[string]any{
 			"success": false,
 			"message": err.Error(),
