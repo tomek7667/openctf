829d1227f22684904263e0026dd4f11b173441a7
tomek7667 <git@cyber-man.pl>
2025-08-14 03:20:24 +0200

    style: adjust font size of rank display in PlaceRow component

--- src/frontend/src/app/contests/[id]/page.tsx
+++ src/frontend/src/app/contests/[id]/page.tsx
@@ -100,7 +100,7 @@ function PlaceRow({
 			);
 		}
 		return (
-			<div className="w-8 h-8 rounded-full bg-primary/20 flex items-center justify-center text-primary font-bold text-lg">
+			<div className="w-8 h-8 rounded-full bg-primary/20 flex items-center justify-center text-primary font-bold text-m">
 				{rank}
 			</div>
 		);
