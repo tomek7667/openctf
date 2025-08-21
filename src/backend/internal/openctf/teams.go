eab35533294c8cc033f3e2b17ce9f93637a392f9
tomek7667 <git@cyber-man.pl>
2025-08-21 03:22:02 +0200

    add list leaderboard more free ratelimit

--- src/backend/internal/openctf/teams.go
+++ src/backend/internal/openctf/teams.go
@@ -2,6 +2,7 @@ package openctf
 
 import (
 	"log/slog"
+	"time"
 
 	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 )
@@ -11,7 +12,10 @@ func (h *Handler) AddRoutes_ApiTeams() {
 
 	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/:teamId", ratelimit.InMemoryOptions{}, h.TeamsGetOne)
 	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/list", ratelimit.InMemoryOptions{}, h.TeamsList)
-	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/leaderboard", ratelimit.InMemoryOptions{}, h.TeamsGetLeaderboard)
+	h.RestClient.AddRateLimitedRoute("GET", "/api/teams/leaderboard", ratelimit.InMemoryOptions{
+		Rate:  time.Minute * 10,
+		Limit: 300,
+	}, h.TeamsGetLeaderboard)
 	// h.RestClient.AddRateLimitedRoute("GET", "/api/teams/ranking", ratelimit.InMemoryOptions{}, h.TeamsRanking) // TODO: before this, create view with aggregated scores for each team each year; this endpoint will receive data from this view; filtered out by provided query params
 	h.RestClient.AddRateLimitedRoute("POST", "/api/teams/create", ratelimit.InMemoryOptions{}, h.WithAuth(h.TeamsCreate))
 	h.RestClient.AddRateLimitedRoute("POST", "/api/teams/verify", ratelimit.InMemoryOptions{}, h.WithAuth(h.TeamsVerify))
